61674279c6aef21835caa3cfdfa42c7a44270b4b
Zahidul Islam Rabbi <132165082+zVoid24@users.noreply.github.com>
2026-04-06 23:50:25 +0600

    Update number_handler.go

--- bot/number_handler.go
+++ bot/number_handler.go
@@ -202,7 +202,7 @@ func (b *Bot) assignNumbers(chatID int64, userID int64, platform, country string
 	number := numbers[0]
 
 	text := fmt.Sprintf(
-		"<b><tg-emoji emoji-id=\"5888699182734122090\">✅</tg-emoji> Number assigned successfully</b>\n\n<b>Assinged Number</b>: <code>%s</code>\n\n<b>Platform:</b> %s\n<b>Country:</b> %s\n\n<i>Waiting for OTP...</i>",
+		"<tg-emoji emoji-id=/'5469931148295547357/'>✅</tg-emoji><b> Number assigned successfully</b>\n\n<b>Assinged Number</b>: <code>%s</code>\n\n<b>Platform:</b> %s\n<b>Country:</b> %s\n\n<i>Waiting for OTP...</i>",
 		number, platform, country,
 	)
 
